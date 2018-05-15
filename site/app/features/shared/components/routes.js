900ffbf4e28a13e527404a287a6c18dcb95e6b3b
Josh Gretz <jgretz@truefit.io>
2018-05-15 11:17:38 -0400

    finish basic routing

--- site/app/features/shared/components/routes.js
+++ site/app/features/shared/components/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
 
+import {About} from '../../about/components';
 import {Admin} from '../../admin/components';
+import {Search} from '../../search/components';
 import NotFound from './notFound';
 
 import {ROUTES} from '../constants';
 
 export default () => (
   <Switch>
+    <Route exact path={ROUTES.home.route} component={Search} />
     <Route path={ROUTES.admin.route} component={Admin} />
+    <Route path={ROUTES.about.route} component={About} />
     <Route component={NotFound} />
   </Switch>
 );
