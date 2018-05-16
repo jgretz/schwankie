24bc381112bdf2bdb795348524c450f5397e6ac9
Josh Gretz <jgretz@truefit.io>
2018-05-16 10:01:13 -0400

    add footer

--- site/app/App.js
+++ site/app/App.js
@@ -3,11 +3,13 @@ import {withRouter} from 'react-router';
 
 import {Bar} from './features/bar/components';
 import {Routes} from './features/shared/components';
+import {Footer} from './features/footer/components';
 
 const App = () => (
-  <div>
+  <div className="wrapper">
     <Bar />
     <Routes />
+    <Footer />
   </div>
 );
 
