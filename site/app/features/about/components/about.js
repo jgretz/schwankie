09c6c28e81e6e7a7aef2546de58b5c338bdd2f56
Josh Gretz <jgretz@truefit.io>
2018-05-16 10:32:55 -0400

    update about

--- site/app/features/about/components/about.js
+++ site/app/features/about/components/about.js
@@ -13,6 +13,14 @@ export default () => (
         categorize and store for the future. That said, I&#39;ve been doing this
         for years, and much of what I&#39;ve indexed lies unfindable in my
         Chrome bookmarks. This is the problem this site is intended to fix.
+        <br />
+        <br />
+        In addition to providing a public, searchable index of my bookmarks, I
+        also wanted to build a better way of categorizing them. With the nature
+        of bookmarks, the only real categorizing you can do is with a lot of
+        nested folder. Rather than follow that paradigm, this site allows me to
+        tag each link as well, adding a layer of knowledge and discoverability
+        that I have been missing.
       </span>
     </p>
   </div>
