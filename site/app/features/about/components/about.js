678b356e15e0f723b0f9e343e5746dfd9ec2f80d
Josh Gretz <jgretz@truefit.io>
2018-05-16 10:14:41 -0400

    add basic about

--- site/app/features/about/components/about.js
+++ site/app/features/about/components/about.js
@@ -1,3 +1,19 @@
 import React from 'react';
 
-export default () => <div>About</div>;
+export default () => (
+  <div className="about">
+    <h1>Schwankie.com</h1>
+    <h3>A random index of the web</h3>
+    <p>
+      <pre>TLDR: It&#39;s a fancy replacement for my browser bookmarks</pre>
+      <span>
+        I read a lot. Whether from Twitter, newsletters, HackerNews, or
+        conferences - a large percentage of my life is spent drinking from the
+        information firehose that is the internet. A lot of what I find, I
+        categorize and store for the future. That said, I&#39;ve been doing this
+        for years, and much of what I&#39;ve indexed lies unfindable in my
+        Chrome bookmarks. This is the problem this site is intended to fix.
+      </span>
+    </p>
+  </div>
+);
