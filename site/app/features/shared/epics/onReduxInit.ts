7e50240a381bff510a7cddabd6cc54e29c9f2c6d
Josh Gretz <jgretz@truefit.io>
2020-04-02 14:02:37 -0400

    add ability to specify search in the query string

--- site/app/features/shared/epics/onReduxInit.ts
+++ site/app/features/shared/epics/onReduxInit.ts
@@ -5,12 +5,20 @@ import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 
 import {APP_INITIALIZED} from '../constants';
+import {setSearchTerm} from '../../search/actions';
 import {loadRecentLinks} from '../../link/actions';
 
 export const onReduxInit: Epic = (action$: Observable<Action>) =>
   action$.pipe(
     ofType(APP_INITIALIZED),
     map(() => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const search = urlParams.get('search');
+
+      if (search) {
+        return setSearchTerm(search);
+      }
+
       return loadRecentLinks();
     }),
   );
