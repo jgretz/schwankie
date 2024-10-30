212f31d1580b98b1f26de83b13c61786f1a0804f
Josh Gretz <jgretz@gmail.com>
2024-10-30 11:02:36 -0400

    style search bar

--- apps/www/app/routes/_index/_components/search.tsx
+++ apps/www/app/routes/_index/_components/search.tsx
@@ -1,8 +1,8 @@
 export default function Search() {
   return (
-    <div className="flex flex-row py-2">
-      <input />
-      <button>Search</button>
+    <div className="flex flex-row pb-3">
+      <input className="w-full rounded-md" />
+      <button className="ml-2 bg-primary text-text rounded-md p-2">Search</button>
     </div>
   );
 }
