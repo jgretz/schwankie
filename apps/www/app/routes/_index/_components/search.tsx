8164821a4ff6540749db13a064d0d901c82fb290
Josh Gretz <jgretz@gmail.com>
2024-10-31 16:11:06 -0400

    update search to accept return

--- apps/www/app/routes/_index/_components/search.tsx
+++ apps/www/app/routes/_index/_components/search.tsx
@@ -14,21 +14,25 @@ export default function Search({initialQuery}: Props) {
     [search],
   );
 
-  const handleSearch = useCallback(() => {
-    console.log(search);
-    window.location.href = `?query=${search}`;
-  }, [search]);
+  const handleSearch = useCallback(
+    (e: {preventDefault: () => void}) => {
+      e.preventDefault();
+
+      window.location.href = `?query=${search}`;
+    },
+    [search],
+  );
 
   return (
-    <div className="flex flex-row pb-3">
+    <form className="flex flex-row pb-3" onSubmit={handleSearch}>
       <input
         className="w-full rounded-md text-primary px-1"
         defaultValue={search}
         onChange={handleSearchTextChange}
       />
-      <button className="ml-2 bg-primary text-text rounded-md p-2" onClick={handleSearch}>
+      <button className="ml-2 bg-primary text-text rounded-md p-2" type="submit">
         Search
       </button>
-    </div>
+    </form>
   );
 }
