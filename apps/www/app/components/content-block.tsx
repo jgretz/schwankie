1d9bc7bdb81d84740304f9aab153de8c8627c539
Josh Gretz <jgretz@gmail.com>
2024-11-05 13:27:25 -0500

    update style for about and admin

--- apps/www/app/components/content-block.tsx
+++ apps/www/app/components/content-block.tsx
@@ -1,5 +1,5 @@
 import type {HasChildrenProps} from '@www/types';
 
 export default function ContentBlock({children}: HasChildrenProps) {
-  return <div className="my-2">{children}</div>;
+  return <div className="my-2 p-3 rounded-md bg-accent">{children}</div>;
 }
--- apps/www/app/routes/admin+/_components/TextInput.tsx
+++ apps/www/app/routes/admin+/_components/TextInput.tsx
@@ -14,9 +14,9 @@ export function TextInput<T extends string | number | null | undefined>({
   return (
     <div className="flex flex-col mt-5 w-full">
       <div className="w-full">
-        <label className="inline-block">{label}</label>
+        <label className="inline-block text-primary">{label}</label>
         <input
-          className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          className="flex h-10 w-full rounded-md border text-primary border-primary bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           type={type || 'text'}
           key={field.key}
           name={field.name}
