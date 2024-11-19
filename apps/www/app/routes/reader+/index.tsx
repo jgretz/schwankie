3f3318cdc4ed5684b4fbe84a7b030f1dda74b76f
Josh Gretz <jgretz@gmail.com>
2024-11-19 13:09:54 -0500

    add return to top button

--- apps/www/app/routes/reader+/index.tsx
+++ apps/www/app/routes/reader+/index.tsx
@@ -9,17 +9,23 @@ import {useCallback, useEffect, useState} from 'react';
 import {FeedList} from './_components/feed-list';
 import CommandBar from './resources+/command-bar';
 import {encodeQueryStringFromJsonObject} from 'utility-util';
+import Spinner from '@www/components/spinner';
+import {queryFeedStats} from '@www/services/domain/feedStats.query';
+import {Button} from '@www/components/ui/button';
 
 type FeedItemResponse = Awaited<ReturnType<typeof fetchFeedItems>>;
 
 async function fetchFeedItems(page: number, includeRead?: boolean, feedId?: number) {
   const feedItems = await queryRssFeedItems(page, includeRead, feedId);
+  const stats = await queryFeedStats();
 
   return {
     page,
     includeRead,
     feedId,
+
     feedItems: feedItems || [],
+    totalFeedItems: stats?.unreadCount ?? 0,
   };
 }
 
@@ -45,24 +51,7 @@ export async function loader({request}: LoaderFunctionArgs) {
 function Working() {
   return (
     <div className="w-full h-screen flex items-center justify-center text-primary">
-      <div role="status">
-        <svg
-          aria-hidden="true"
-          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-      </div>
+      <Spinner />
     </div>
   );
 }
@@ -75,12 +64,29 @@ function NoContent() {
   );
 }
 
+function ReturnToTopButton({show}: {show: boolean}) {
+  const returnToTop = useCallback(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="w-full flex flex-row items-center justify-center mt-[-70px] mb-1">
+      <Button onClick={returnToTop}>Return To Top</Button>
+    </div>
+  );
+}
+
 export default function RSS() {
   // feed items
   const initialData = useLoaderData<FeedItemResponse>();
   const fetcher = useFetcher<FeedItemResponse>();
 
   const [feedItems, setFeedItems] = useState(initialData.feedItems);
+  const [totalFeedItems, setTotalFeedItems] = useState(initialData.totalFeedItems);
 
   useEffect(() => {
     if (!fetcher.data || fetcher.state === 'loading') {
@@ -90,6 +96,9 @@ export default function RSS() {
     if (fetcher.data) {
       const newFeedItems = fetcher.data.feedItems;
       setFeedItems((prevItems) => [...prevItems, ...newFeedItems]);
+
+      const newTotalFeedItems = fetcher.data.totalFeedItems;
+      setTotalFeedItems(newTotalFeedItems);
     }
   }, [fetcher.data]);
 
@@ -117,6 +126,7 @@ export default function RSS() {
   // derived state
   const loading = fetcher.state === 'loading';
   const mostRecentItemId = Math.max(...feedItems.map((x) => x.id || 0));
+  const showBottomButton = totalFeedItems > 0 && feedItems.length >= totalFeedItems;
 
   // JSX
   const List = (
@@ -135,6 +145,7 @@ export default function RSS() {
     <div>
       <CommandBar refresh={refresh} setWorking={setWorking} mostRecentItemId={mostRecentItemId} />
       {Content}
+      <ReturnToTopButton show={showBottomButton} />
     </div>
   );
 }
