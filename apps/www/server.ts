129751b7bd74e3e3440a9ab3694d8c3f48834b7c
Josh Gretz <jgretz@gmail.com>
2026-03-17 16:25:03 -0400

    fix(www): serve all static files from dist/client before SSR handler

--- apps/www/server.ts
+++ apps/www/server.ts
@@ -16,13 +16,11 @@ async function initializeServer() {
     port: SERVER_PORT,
 
     routes: {
-      '/assets/*': async (req: Request) => {
+      '/*': async (req: Request) => {
         const url = new URL(req.url);
         const file = Bun.file(path.join(CLIENT_DIRECTORY, url.pathname));
         if (await file.exists()) return new Response(file);
-        return new Response('Not found', {status: 404});
-      },
-      '/*': (req: Request) => {
+
         try {
           return handler.fetch(req);
         } catch (error) {
